e7e5db648d142d6624c35d411b41aefc242f4e72
nojhi3 <akshatrai303@gmail.com>
2024-06-17 20:06:32 +0530

    commit1

---
+++ constants.py
@@ -0,0 +1,4 @@
+#openai key name - key1me
+gemini_key = "AIzaSyDqWGIhY3snEV0dlGdUXERbrh88g-gbOHo"
+#Please generate your own api key, this only has limit of 1 mil tokens.
+#It will run out if we all use the same.
---
+++ main.py
@@ -0,0 +1,50 @@
+from constants import gemini_key
+import streamlit as st
+import google.generativeai as genai
+
+
+genai.configure(api_key=gemini_key)
+
+model = genai.GenerativeModel(
+    "gemini-1.5-pro",
+    system_instruction=[
+        "You are a very flirty person.",
+        "you respond as if you want to impress the person in front.",
+        "You only respond in english"
+    ],
+    )
+
+
+def get_gemini_response(question):
+    response = model.generate_content(
+        question,
+        safety_settings={
+            "HARM_CATEGORY_HARASSMENT": "block_none",
+            "HARM_CATEGORY_SEXUALLY_EXPLICIT": "block_none",
+            "HARM_CATEGORY_HATE_SPEECH": "block_none",
+            "HARM_CATEGORY_DANGEROUS_CONTENT": "block_none",
+        },
+        generation_config=genai.GenerationConfig(
+            candidate_count=1,
+            temperature=0.9,
+            max_output_tokens=500
+        )
+        )
+    response.prompt_feedback
+    return response.text
+
+st.set_page_config(page_title="q and a")
+st.header("Gemini llm")
+input = st.text_input("Input: ", key="input")
+submit = st.button("ask")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("Response: ")
+    st.write(response)
+
+
+
+
+
+
