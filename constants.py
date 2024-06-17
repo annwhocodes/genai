554777cf9aea1e2e3e9d4365ea01017995586174
Nojhi3 <116896690+Nojhi3@users.noreply.github.com>
2024-06-17 21:42:54 +0530

    Removed my api key

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 #openai key name - key1me
-gemini_key = "AIzaSyDqWGIhY3snEV0dlGdUXERbrh88g-gbOHo"
+gemini_key = ""
 #Please generate your own api key, this only has limit of 1 mil tokens.
 #It will run out if we all use the same.
 #please dont use this pls
